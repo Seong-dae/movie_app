dc3fe38dcb94ecf4cd4c74dc1476a6220d3cad45
Seong-dae <42955124+Seong-dae@users.noreply.github.com>
2020-02-11 17:40:04 +0900

    Updates

---
+++ precache-manifest.2e5065e3c8e0a010cc7c805e515979de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87efccc4bf8f838a33a67d5c5604adef",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "ddb71c2d68e3875486cd",
+    "url": "/movie_app/static/css/main.b7cf6848.chunk.css"
+  },
+  {
+    "revision": "70e84318d65e22caa855",
+    "url": "/movie_app/static/js/2.1c932c0d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.1c932c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddb71c2d68e3875486cd",
+    "url": "/movie_app/static/js/main.eaaa97bc.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
