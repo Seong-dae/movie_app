8d87ff5b847f1d7c98880a4cd936e382b3a17734
Seong-dae <42955124+Seong-dae@users.noreply.github.com>
2020-02-11 15:21:29 +0900

    component Life Cycle

--- src/App.js
+++ src/App.js
@@ -7,15 +7,28 @@ import PropTypes from 'prop-types';
 //리액트는 자동적으로 class컴포넌트의 render메소드를 실행한다 !!!!!!!!!!
 
 class App extends React.Component{ // 리액트컴포넌트!!!!
+  constructor(props){ // 렌더 이전에 호출됨. (순서 : constructor -> ~ -> render -> componentDidMount
+    super(props);
+    console.log("hello Im constructor")
+  }
   state = {//데이터가 변하기때문에 state를 사용함, state는 클래스컴포넌트의 기능
     count: 0
   }
   //setState를통해서 페이지 새로고침이 아니라 render만 refresh해준다. 굿
-  add = () => {this.setState(current => ({count: current.count + 1}))} // current지원!
+  add = () => {this.setState(current => ({count: current.count + 1})); console.log("add!")} // current지원!
 
   minus = () => {this.setState({count: this.state.count - 1})}
-
+  componentDidMount(){ // render이후에 호출
+    console.log("component Mounted!!!!")
+  }
+  componentDidUpdate(){
+    console.log("componentDid Update!")
+  }
+  componentWillUnmount(){
+    console.log("good bye...")
+  }
   render(){
+    console.log("Im render")
     return(
       <div>
         <h1>The number is {this.state.count}</h1>
